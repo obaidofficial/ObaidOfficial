6fe411cfd68afbb403f7dfb9d804326c6544ae58
Obaid Ullah <itsobaidofficial@gmail.com>
2026-01-28 17:08:25 +0500

    About me Fonts Update

--- components/Home/AboutMe/AboutMe.tsx
+++ components/Home/AboutMe/AboutMe.tsx
@@ -10,21 +10,21 @@ const AboutMe = () => {
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 lg:gap-15">
           <div className="lg:col-span-1 p-1 flex flex-col items-left justify-center">
             <h2 className="text-6xl font-medium mb-4 text-[#161616] dark:text-[#fcfcfc] tracking-[-2px]">
-              Hi, I am <span className="font-bold uppercase">Obaid Ullah</span>
+              Hi, I am <span className="font-bold">Obaid Ullah</span>
             </h2>
-            <p className="text-[14px] mb-4 md:mb-2 w-full md:text-[18px] leading-[1.5em] text-[#161616] dark:text-[#fcfcfc] font-thin">
+            <p className="text-[16px] mb-4 md:mb-2 w-full md:text-[18px] leading-[1.5em] text-[#161616] dark:text-[#fcfcfc] font-thin">
               I am a Website Developer, I have delivered about 200+ Website in
               my 5+ Years of Experience. My priority for website is Full Fledge
               functionable website with Optimized Speed & Eye Catching Modern
               Layout.
             </p>
-            <p className="text-[14px] mb-4 md:mb-2 w-full md:text-[18px] leading-[1.5em] text-[#161616] dark:text-[#fcfcfc] font-thin">
+            <p className="text-[16px] mb-4 md:mb-2 w-full md:text-[18px] leading-[1.5em] text-[#161616] dark:text-[#fcfcfc] font-thin">
               I have delivered the websites approximately in every Category,
               like E-Commerce Store, Learning Management System (LMS), Blog,
               Business, Portfolio with Single Page Website (Landing page) to
               Multi Page website.
             </p>
-            <p className="text-[14px] mb-4 md:mb-7 w-full md:text-[18px] leading-[1.5em] text-[#161616] dark:text-[#fcfcfc] font-thin">
+            <p className="text-[16px] mb-4 md:mb-7 w-full md:text-[18px] leading-[1.5em] text-[#161616] dark:text-[#fcfcfc] font-thin">
               My Speciality is in WordPress, Shopify, Square Space, Kajabi, Wix
               and Framer, etc. I also use custom code for different websites
               which includes php, Tailwind CSS, Vanila CSS, HTML, JavaScript,
