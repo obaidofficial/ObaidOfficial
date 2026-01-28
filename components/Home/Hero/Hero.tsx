6a4bd40365e00a1510d83da58c61af181cac7dcb
Obaid Ullah <itsobaidofficial@gmail.com>
2026-01-28 18:24:36 +0500

    Reverse Screen Size

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="relative w-full md:h-[98vh] sm:h-screen bg-[url('/images/hero/heroLight.webp')] dark:bg-[url('/images/hero/heroDark.webp')] bg-cover bg-no-repeat bg-center">
+    <div className="relative w-full md:h-[98vh] h-[88vh] sm:h-screen bg-[url('/images/hero/heroLight.webp')] dark:bg-[url('/images/hero/heroDark.webp')] bg-cover bg-no-repeat bg-center">
       {/* Overlay */}
       <div className="absolute top-0 left-0 w-full h-full  bg-[radial-gradient(50%_50%_at_50%_50%,#fff0_0%,#fcfcfc_100%)] dark:bg-[radial-gradient(50%_50%_at_50%_50%,#0000_0%,#090909_100%)] "></div>
       {/* Text Content */}
